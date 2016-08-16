9972f33d97d5644dcac7e278d01f19f6e22b6f14
xiaolei.jin <jinxiaolei@le.com>
2016-08-16 14:18:08 +0800

    添加不可变数组越界保护

--- Example/XSafeAccess/XSafeAccessViewController.m
+++ Example/XSafeAccess/XSafeAccessViewController.m
@@ -14,16 +14,16 @@
 
 @implementation XSafeAccessViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    
+    NSArray *array = @[];
+    
+    [array objectAtIndex:98];
 }
 
 @end
---
+++ XSafeAccess/Classes/NSArray+XSafe.h
@@ -0,0 +1,15 @@
+//
+//  NSArray+XSafe.h
+//  Pods
+//
+//  Created by 金小白 on 16/8/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (XSafe)
+
+- (id)safeObjectAtIndex:(NSInteger)index;
+
+@end
