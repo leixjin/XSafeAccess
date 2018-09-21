5f170ff1c1854274ff7b3e85cad4de4d5cf1c7fb
金小白 <xiaolei.jin@okcoin.com>
2018-09-21 16:37:09 +0800

    添加测试用例

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -14,22 +14,323 @@
 
 @implementation Tests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)tearDown {
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)testExample {
+    [self arrayTest];
+    [self array2Test];
+    [self array3Test];
+    
+    [self mArrayTest];
+    [self mArrayTest2];
+    [self mArrayTest3];
+    
+    [self emptyArrayTest];
+    [self emptyMArrayTest];
+    
+    [self dictionaryTest];
+    [self dictionaryTest2];
+    
+    [self mDictionaryTest];
+    [self mDictionaryTest2];
+    
+    [self emptyDictionaryTest];
+    [self emptyMDictionaryTest];
+}
+
+#pragma mark - Array
+
+- (void)arrayTest {
+    NSArray *array = @[@1];
+    
+    [array objectAtIndex:1];
+    __unused id xx = array[1];
+    
+    NSArray *resultArray = [array subarrayWithRange:NSMakeRange(0, 1)];
+    XCTAssert([resultArray isEqualToArray:array], @"subarray 实现错误");
+    
+    NSArray *emptyArray0 = [array subarrayWithRange:NSMakeRange(1, 1)];
+    XCTAssert(emptyArray0 == nil, @"subarray 实现错误");
+    
+    NSArray *emptyArray1 = [array subarrayWithRange:NSMakeRange(1, 0)];
+    XCTAssert([emptyArray1 isEqualToArray:@[]], @"subarray 实现错误");
+}
+
+- (void)array2Test {
+    NSArray *array = @[@1, @2];
+    
+    [array objectAtIndex:1];
+    __unused id xx = array[2];
+    
+    NSArray *resultArray = [array subarrayWithRange:NSMakeRange(0, 2)];
+    XCTAssert([resultArray isEqualToArray:array], @"subarray 实现错误");
+    
+    NSArray *emptyArray0 = [array subarrayWithRange:NSMakeRange(1, 2)];
+    XCTAssert(emptyArray0 == nil, @"subarray 实现错误");
+    
+    NSArray *emptyArray1 = [array subarrayWithRange:NSMakeRange(1, 0)];
+    XCTAssert([emptyArray1 isEqualToArray:@[]], @"subarray 实现错误");
+}
+
+- (void)array3Test {
+    UIView *view = [UIView new];
+    NSArray *array = @[@1, view];
+    
+    [array objectAtIndex:1];
+    __unused id xx = array[2];
+    
+    NSArray *resultArray = [array subarrayWithRange:NSMakeRange(0, 2)];
+    XCTAssert([resultArray isEqualToArray:array], @"subarray 实现错误");
+    
+    NSArray *emptyArray0 = [array subarrayWithRange:NSMakeRange(1, 2)];
+    XCTAssert(emptyArray0 == nil, @"subarray 实现错误");
+    
+    NSArray *emptyArray1 = [array subarrayWithRange:NSMakeRange(1, 0)];
+    XCTAssert([emptyArray1 isEqualToArray:@[]], @"subarray 实现错误");
+}
+
+- (void)emptyArrayTest {
+    NSArray *emptyArray = @[];
+    
+    [emptyArray objectAtIndex:1];
+    __unused id xx = emptyArray[1];
+    
+    NSArray *resultArray = [emptyArray subarrayWithRange:NSMakeRange(0, 1)];
+    XCTAssert(resultArray == nil, @"subarray 实现错误");
+    
+    NSArray *emptyArray0 = [emptyArray subarrayWithRange:NSMakeRange(1, 1)];
+    XCTAssert(emptyArray0 == nil, @"subarray 实现错误");
+    
+    NSArray *emptyArray1 = [emptyArray subarrayWithRange:NSMakeRange(1, 0)];
+    XCTAssert(emptyArray1 == nil, @"subarray 实现错误");
+}
+
+#pragma mark - MutableArray
+
+- (void)mArrayTest {
+    NSMutableArray *mArray = @[@1].mutableCopy;
+    
+    [mArray objectAtIndex:1];
+    [mArray addObject:nil];
+    [mArray addObject:NULL];
+    [mArray addObject:[NSNull new]];
+    
+    [mArray removeObject:nil];
+    [mArray removeObject:[NSObject new]];
+    
+    [mArray replaceObjectAtIndex:1 withObject:nil];
+    [mArray replaceObjectAtIndex:100 withObject:[NSObject new]];
+}
+
+- (void)mArrayTest2 {
+    NSMutableArray *mArray = @[@1, @2].mutableCopy;
+    
+    [mArray objectAtIndex:1];
+    [mArray addObject:nil];
+    [mArray addObject:NULL];
+    [mArray addObject:[NSNull new]];
+    
+    [mArray removeObject:nil];
+    [mArray removeObject:[NSObject new]];
+    
+    [mArray replaceObjectAtIndex:1 withObject:nil];
+    [mArray replaceObjectAtIndex:100 withObject:[NSObject new]];
+}
+
+- (void)mArrayTest3 {
+    UIView *view = [UIView new];
+    NSMutableArray *mArray = @[@1, view].mutableCopy;
+    
+    [mArray objectAtIndex:1];
+    [mArray addObject:nil];
+    [mArray addObject:NULL];
+    [mArray addObject:[NSNull new]];
+    
+    [mArray removeObject:nil];
+    [mArray removeObject:[NSObject new]];
+    
+    [mArray replaceObjectAtIndex:1 withObject:nil];
+    [mArray replaceObjectAtIndex:100 withObject:[NSObject new]];
+}
+
+- (void)emptyMArrayTest {
+    NSMutableArray *emptyMArray = @[].mutableCopy;
+    
+    [emptyMArray objectAtIndex:1];
+    [emptyMArray addObject:nil];
+    [emptyMArray addObject:NULL];
+    [emptyMArray addObject:[NSNull new]];
+    
+    [emptyMArray removeObject:nil];
+    [emptyMArray removeObject:[NSObject new]];
+    
+    [emptyMArray replaceObjectAtIndex:1 withObject:nil];
+    [emptyMArray replaceObjectAtIndex:100 withObject:[NSObject new]];
+}
+
+#pragma mark - Dictionary
+
+- (void)dictionaryTest {
+    NSDictionary *dic = @{@"key":@"value"};
+    [dic setValue:@"xx" forKey:@"key"];//设置key
+    XCTAssert([dic isEqualToDictionary:@{@"xx":@"value"}], @"dic 实现错误");
+    
+    [dic setValue:@"xx" forKey:@"newkey"];
+    XCTAssert([dic isEqualToDictionary:@{@"xx":@"value"}], @"dic 实现错误");
+
+    [dic setValue:nil forKey:@"key"];
+    XCTAssert([dic isEqualToDictionary:@{@"xx":@"value"}], @"dic 实现错误");
+    
+    id v = [dic valueForKey:nil];
+    XCTAssert(v == nil, @"dic 实现错误");
+    
+    id vv = [dic objectForKey:@"xx"];
+    XCTAssert([vv isEqualToString:@"value"], @"dic 实现错误");
+    
+    id vvv = [dic objectForKey:nil];
+    XCTAssert(vvv == nil, @"dic 实现错误");
+}
+
+- (void)dictionaryTest2 {
+    NSDictionary *dic = @{@"key":@"value", @"key2":@"value2"};
+
+    id v = [dic valueForKey:nil];
+    XCTAssert(v == nil, @"dic 实现错误");
+
+    id vv = [dic objectForKey:@"xx"];
+    XCTAssert(vv == nil, @"dic 实现错误");
+
+    id vvv = [dic objectForKey:nil];
+    XCTAssert(vvv == nil, @"dic 实现错误");
+}
+
+- (void)emptyDictionaryTest {
+    NSDictionary *dic = @{};
+    [dic setValue:@"xx" forKey:@"key"];//设置key
+    XCTAssert([dic isEqualToDictionary:@{}], @"dic 实现错误");
+    
+    [dic setValue:@"xx" forKey:@"newkey"];
+    XCTAssert([dic isEqualToDictionary:@{}], @"dic 实现错误");
+    
+    [dic setValue:nil forKey:@"key"];
+    XCTAssert([dic isEqualToDictionary:@{}], @"dic 实现错误");
+    
+    id v = [dic valueForKey:nil];
+    XCTAssert(v == nil, @"dic 实现错误");
+    
+    id vv = [dic objectForKey:@"xx"];
+    XCTAssert(vv == nil, @"dic 实现错误");
+    
+    id vvv = [dic objectForKey:nil];
+    XCTAssert(vvv == nil, @"dic 实现错误");
+}
+
+#pragma mark - Mutable Dictionary
+
+- (void)mDictionaryTest {
+    NSMutableDictionary *mDic = @{@"key":@"value"}.mutableCopy;
+
+    [mDic setObject:nil forKey:@"key"];
+    XCTAssert([mDic isEqualToDictionary:@{@"key":@"value"}], @"mDic 实现错误");
+    
+    [mDic setObject:@"value" forKey:nil];
+    XCTAssert([mDic isEqualToDictionary:@{@"key":@"value"}], @"mDic 实现错误");
+    
+    [mDic setObject:nil forKey:nil];
+    XCTAssert([mDic isEqualToDictionary:@{@"key":@"value"}], @"mDic 实现错误");
+
+    [mDic setObject:nil forKey:@"newKey"];
+    XCTAssert([mDic isEqualToDictionary:@{@"key":@"value"}], @"mDic 实现错误");
+    
+    [mDic setObject:@"newValue" forKey:@"newKey"];
+    NSDictionary *d = @{@"key":@"value", @"newKey":@"newValue"};
+    XCTAssert([mDic isEqualToDictionary:d], @"mDic 实现错误");
+
+    [mDic setObject:[NSNull new] forKey:@"newKey"];
+    NSDictionary *dd = @{@"key":@"value", @"newKey":[NSNull new]};
+    XCTAssert([mDic isEqualToDictionary:dd], @"mDic 实现错误");
+    
+    id v = [mDic objectForKey:@"undefine"];
+    XCTAssert(v == nil, @"mDic 实现错误");
+    
+    id vv = [mDic objectForKey:@"key"];
+    XCTAssert([vv isEqualToString:@"value"], @"mDic 实现错误");
+    
+    [mDic removeObjectForKey:@"undefine"];
+    [mDic valueForKey:@"undefine"];
+}
+
+- (void)mDictionaryTest2 {
+    NSMutableDictionary *mDic = @{@"key":@"value", @"key2":@"value2"}.mutableCopy;
+    NSDictionary *originD = @{@"key":@"value", @"key2":@"value2"};
+    
+    [mDic setObject:nil forKey:@"key"];
+    XCTAssert([mDic isEqualToDictionary:originD], @"mDic 实现错误");
+    
+    [mDic setObject:@"value" forKey:nil];
+    XCTAssert([mDic isEqualToDictionary:originD], @"mDic 实现错误");
+    
+    [mDic setObject:nil forKey:nil];
+    XCTAssert([mDic isEqualToDictionary:originD], @"mDic 实现错误");
+    
+    [mDic setObject:nil forKey:@"newKey"];
+    XCTAssert([mDic isEqualToDictionary:originD], @"mDic 实现错误");
+    
+    [mDic setObject:@"newValue" forKey:@"newKey"];
+    NSDictionary *d = @{@"key":@"value", @"newKey":@"newValue", @"key2":@"value2"};
+    XCTAssert([mDic isEqualToDictionary:d], @"mDic 实现错误");
+    
+    [mDic setObject:[NSNull new] forKey:@"newKey"];
+    NSDictionary *dd = @{@"key":@"value", @"newKey":[NSNull new], @"key2":@"value2"};
+    XCTAssert([mDic isEqualToDictionary:dd], @"mDic 实现错误");
+    
+    id v = [mDic objectForKey:@"undefine"];
+    XCTAssert(v == nil, @"mDic 实现错误");
+    
+    id vv = [mDic objectForKey:@"key"];
+    XCTAssert([vv isEqualToString:@"value"], @"mDic 实现错误");
+    
+    [mDic removeObjectForKey:@"undefine"];
+    [mDic valueForKey:@"undefine"];
+}
+
+- (void)emptyMDictionaryTest {
+    NSMutableDictionary *mDic = @{}.mutableCopy;
+    
+    [mDic setObject:nil forKey:@"key"];
+    XCTAssert([mDic isEqualToDictionary:@{}], @"mDic 实现错误");
+    
+    [mDic setObject:@"value" forKey:nil];
+    XCTAssert([mDic isEqualToDictionary:@{}], @"mDic 实现错误");
+    
+    [mDic setObject:nil forKey:nil];
+    XCTAssert([mDic isEqualToDictionary:@{}], @"mDic 实现错误");
+    
+    [mDic setObject:nil forKey:@"newKey"];
+    XCTAssert([mDic isEqualToDictionary:@{}], @"mDic 实现错误");
+    
+    [mDic setObject:@"newValue" forKey:@"newKey"];
+    NSDictionary *d = @{@"newKey":@"newValue"};
+    XCTAssert([mDic isEqualToDictionary:d], @"mDic 实现错误");
+    
+    [mDic setObject:[NSNull new] forKey:@"newKey"];
+    NSDictionary *dd = @{@"newKey":[NSNull new]};
+    XCTAssert([mDic isEqualToDictionary:dd], @"mDic 实现错误");
+    
+    id v = [mDic objectForKey:@"undefine"];
+    XCTAssert(v == nil, @"mDic 实现错误");
+    
+    id vv = [mDic objectForKey:@"key"];
+    XCTAssert(vv == nil, @"mDic 实现错误");
+    
+    [mDic removeObjectForKey:@"undefine"];
+    [mDic valueForKey:@"undefine"];
 }
 
 @end
-
--- XSafeAccess/Classes/XSafeAccess.m
+++ XSafeAccess/Classes/XSafeAccess.m
@@ -14,7 +14,6 @@
 @implementation NSObject (XSafeAccessSwizzle)
 
 + (BOOL)swizzleInstanceMethod:(SEL)originalSel originClass:(Class)originCls with:(SEL)newSel newClass:(Class)newCls {
-    
     Method originalMethod = class_getInstanceMethod(originCls, originalSel);
     Method newMethod = class_getInstanceMethod(newCls, newSel);
     
@@ -35,8 +34,11 @@
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
+        [self swizzleInstanceMethod:@selector(subarrayWithRange:) originClass:NSClassFromString(@"NSArray") with:@selector(safeSubarrayWithRange:) newClass:self];
         [self swizzleInstanceMethod:@selector(objectAtIndex:) originClass:NSClassFromString(@"__NSArrayI") with:@selector(safeObjectAtIndexI:) newClass:self];
+        [self swizzleInstanceMethod:@selector(objectAtIndexedSubscript:) originClass:NSClassFromString(@"__NSArrayI") with:@selector(safeObjectAtIndexedSubscript:) newClass:self];
         [self swizzleInstanceMethod:@selector(objectAtIndex:) originClass:NSClassFromString(@"__NSArray0") with:@selector(safeObjectAtIndex0:) newClass:self];
+        [self swizzleInstanceMethod:@selector(objectAtIndex:) originClass:NSClassFromString(@"__NSSingleObjectArrayI") with:@selector(safeSingleObjectAtIndexI:) newClass:self];
     });
 }
 
@@ -52,6 +54,33 @@
 - (id)safeObjectAtIndex0:(NSUInteger)index {
     if (index < self.count) {
         return [self safeObjectAtIndex0:index];
+    }else {
+        XSafeAssert(NO, @"Index is beyond bounds!!! 数组越界！！！");
+        return nil;
+    }
+}
+
+- (id)safeSingleObjectAtIndexI:(NSUInteger)index {
+    if (index < self.count) {
+        return [self safeSingleObjectAtIndexI:index];
+    }else {
+        XSafeAssert(NO, @"Index is beyond bounds!!! 数组越界！！！");
+        return nil;
+    }
+}
+
+- (id)safeObjectAtIndexedSubscript:(NSUInteger)idx {
+    if (idx < self.count) {
+        return [self safeObjectAtIndexedSubscript:idx];
+    }else {
+        XSafeAssert(NO, @"Index is beyond bounds!!! 数组越界！！！");
+        return nil;
+    }
+}
+
+- (NSArray *)safeSubarrayWithRange:(NSRange)range {
+    if (range.location + range.length <= self.count) {
+        return [self safeSubarrayWithRange:range];
     }else {
         XSafeAssert(NO, @"Index is beyond bounds!!! 数组越界！！！");
         return nil;
@@ -102,6 +131,73 @@
         [self safeReplaceObjectAtIndexM:index withObject:anObject];
     }else {
         XSafeAssert(NO, @"Index is beyond bounds or object is nil!!! 可变数组替换保护！！！");
+    }
+}
+
+@end
+
+
+@implementation NSDictionary (XSafeAccess)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [self swizzleInstanceMethod:@selector(objectForKey:) originClass:NSClassFromString(@"__NSSingleEntryDictionaryI") with:@selector(safeObjectForKey:) newClass:self];
+        [self swizzleInstanceMethod:@selector(setValue:forKey:) originClass:NSClassFromString(@"__NSSingleEntryDictionaryI") with:@selector(safeSingleEntryDictionaryISetValue:forKey:) newClass:self];
+        [self swizzleInstanceMethod:@selector(setValue:forKey:) originClass:NSClassFromString(@"__NSDictionary0") with:@selector(safeDictionary0SetValue:forKey:) newClass:self];
+    });
+}
+
+- (id)safeObjectForKey:(id)aKey {
+    if (aKey) {
+        return [self safeObjectForKey:aKey];
+    }else {
+        XSafeAssert(NO, @"Dictionary key is nil!!! 字典空 key 保护！！！");
+        return nil;
+    }
+}
+
+- (void)safeSingleEntryDictionaryISetValue:(id)value forKey:(NSString *)key {
+    if ([self.allKeys containsObject:key]) {
+        if (value) {
+            [self safeSingleEntryDictionaryISetValue:value forKey:key];
+        }else {
+            XSafeAssert(NO, @"Dictionary value is nil!!! 字典空 value 保护！！！");
+        }
+    }else {
+        XSafeAssert(NO, @"Dictionary key is undefine !!! 字典 key 未定义保护！！！");
+    }
+}
+
+- (void)safeDictionary0SetValue:(id)value forKey:(NSString *)key {
+    if ([self.allKeys containsObject:key]) {
+        if (value) {
+            [self safeDictionary0SetValue:value forKey:key];
+        }else {
+            XSafeAssert(NO, @"Dictionary value is nil!!! 字典空 value 保护！！！");
+        }
+    }else {
+        XSafeAssert(NO, @"Dictionary key is undefine !!! 字典 key 未定义保护！！！");
+    }
+}
+
+@end
+
+
+@implementation NSMutableDictionary (XSafeAccess)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [self swizzleInstanceMethod:@selector(setObject:forKey:) originClass:NSClassFromString(@"__NSDictionaryM") with:@selector(safeSetObject:forKey:) newClass:self];
+    });
+}
+
+- (void)safeSetObject:(id)anObject forKey:(id<NSCopying>)aKey {
+    if (anObject && aKey) {
+        [self safeSetObject:anObject forKey:aKey];
+    }else {
+        XSafeAssert(NO, @"Dictionary key/value is nil !!! 字典空 key/value 保护！！！");
     }
 }
 
